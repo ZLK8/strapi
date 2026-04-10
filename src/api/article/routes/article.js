178174507393dafaf9d1b2d5a5ede31b61c4d791
ZLK8 <abc69945341@gmail.com>
2026-04-10 18:28:51 +0800

    Update article.js

--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -1,3 +1,16 @@
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::article.article');
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/articles',
+      handler: 'api::article.article.find',
+      config: { policies: [], auth: false },
+    },
+    {
+      method: 'GET',
+      path: '/articles/:id',
+      handler: 'api::article.article.findOne',
+      config: { policies: [], auth: false },
+    },
+  ],
+};
