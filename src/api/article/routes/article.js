defc45a31cddae139440114d188b4b479e32606e
ZLK8 <abc69945341@gmail.com>
2026-04-10 18:46:52 +0800

    Create article.js

---
+++ src/api/article/routes/article.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * article router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::article.article', {
+  config: {
+    find: { auth: false },
+    findOne: { auth: false },
+  },
+});
